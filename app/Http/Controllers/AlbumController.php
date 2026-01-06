304535fb1cc252a18e9ac1ae1020dfb49f062607
rommel dalisay <rommelmdalisay@gmail.com>
2026-01-06 11:23:23 +0800

    album seeder

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Album;
 
 class AlbumController extends Controller
 {
@@ -11,7 +12,8 @@ class AlbumController extends Controller
      */
     public function index()
     {
-        //
+        $albums = Album::all();
+        return view('album.index', compact('albums'));
     }
 
     /**
