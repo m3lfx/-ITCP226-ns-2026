efac01ee85335f24a7d2bd57106aa8608fdf1f1d
rommel dalisay <rommelmdalisay@gmail.com>
2026-01-06 08:26:34 +0800

    first commit

---
+++ app/Http/Controllers/ArtistController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Artist;
+
+class ArtistController extends Controller
+{
+    // Route::get('/artists', [ArtistController::class, 'index']);
+    public function index()
+    {
+        $artists = Artist::all();
+        // dd($artists);
+        return view('artist.index', compact('artists'));
+    }
+    // Route::get('/artists/create', 
+    // [ArtistController::class, 'create']);
+    public function create()
+    {
+        return view('artist.create');
+    }
+    // Route::post('/artists', 
+    // [ArtistController::class, 'store']);
+    public function store(Request $request)
+    {
+        // dd($request->artist_name, $request->country);
+        $artist = new Artist();
+        $artist->name = trim($request->artist_name);
+        $artist->country = $request->country;
+        $artist->img_path = $request->image;
+        $artist->save();
+    }
+}
