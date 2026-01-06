1a9958477c97b176f128cf3268ee294f51c227f1
rommel dalisay <rommelmdalisay@gmail.com>
2026-01-06 08:32:44 +0800

    first commit

--- resources/views/artist/index.blade.php
+++ resources/views/artist/index.blade.php
@@ -1,46 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.base')
+@section('body')
+    <div><a class="btn btn-primary " href="{{ url('/artist/create') }}" aria-disabled="true">create Artist</a></div>
+    <table class="table table-striped table-hover">
+        @foreach ($artists as $artist)
+            <tr>
+                <td>{{ $artist->id }}</td>
+                <td>{{ $artist->name }}</td>
+                <td>{{ $artist->country }}</td>
+                <td>{{ $artist->img_path }}</td>
+                <td><a href="{{ url('/artists/' . $artist->id . '/edit') }}"><i class="fas fa-edit"></i></a></td>
+                <td><a href="{{ url('/artists/' . $artist->id . '/delete') }}"><i class="fas fa-trash"
+                            style="color:red"></i></a>
+                </td>
+            </tr>
+        @endforeach
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bootstrap Table Example</title>
-    <!-- Include Bootstrap CSS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.8/js/bootstrap.min.js"
-        integrity="sha512-nKXmKvJyiGQy343jatQlzDprflyB5c+tKCzGP3Uq67v+lmzfnZUi/ZT+fc6ITZfSC5HhaBKUIvr/nTLCV+7F+Q=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.8/css/bootstrap.min.css"
-        integrity="sha512-2bBQCjcnw658Lho4nlXJcc6WkV/UxpE/sAokbXPxQNGqmNdQrWqtw26Ns9kFF/yG792pKR1Sx8/Y1Lf1XN4GKA=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-</head>
-
-<body>
-    {{-- {{ dd($artists) }} --}}
-    <div class="container mt-4">
-        <h2>artist</h2>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">id</th>
-                    <th scope="col">artist name</th>
-                    <th scope="col">country</th>
-                    <th scope="col">image</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($artists as $artist)
-                    <tr>
-                        <td>{{ $artist->id }}</td>
-                        <td>{{ $artist->name }}</td>
-                        <td>{{ $artist->country }}</td>
-                        <td>{{ $artist->img_path }}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
-
-
-</body>
-
-</html>
+    </table>
+@endsection
