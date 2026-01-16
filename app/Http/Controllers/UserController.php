d09fb6fccb8f04b6cae7bd8441ce11cf61ccf0ae
rommel dalisay <rommelmdalisay@gmail.com>
2026-01-16 08:19:10 +0800

    user auth

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Validator;
+use Auth;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        // dd(Hash::make($request->password));
+        // dd($request->all());
+        // $validated = $request->validate([
+        //     'email' => 'required|email',
+        //     'password' => 'required|min:6',
+        //     'lname' => 'required|alpha',
+        //     'img_path' => 'required'
+        // ]);
+        $rules = [
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+            'lname' => 'required|alpha',
+            'img_path' => 'required|mimes:jpg,png'
+        ];
+        $messages = [
+            'required' => 'The :attribute ay may content',
+            'email' => 'ang :attribute format ay mali kamote ka',
+            'password' => 'dapat anim o mahigit na characters',
+            'email.required' => 'ilagay mo email mo'
+        ];
+
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        if ($validator->fails()) {
+            return redirect()->back()
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        $user = User::create([
+            'email' => trim($request->email),
+            'password' => bcrypt($request->password),
+            'name' => $request->fname . " " . $request->lname
+        ]);
+
+        // $path = $request->file('img_path')->store('images');
+
+        $path = $request->file('img_path')->storeAs(
+            'public/images',
+            $request->file('img_path')->hashName()
+        );
+        // dd($path);
+        $listener = Listener::create([
+            'fname' => trim($request->fname),
+            'lname' => trim($request->lname),
+            'address' => $request->address,
+            'img_path' => $path,
+            'user_id' => $user->id
+
+        ]);
+
+        // dd($listener);
+        Auth::login($user);
+        return redirect()->route('user.profile');
+    }
+}
