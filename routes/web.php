3c1af38b1776cc7192e1aa74c5df6081daa8a749
rommel dalisay <rommelmdalisay@gmail.com>
2026-01-16 09:57:35 +0800

    authentication

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/artists', [ArtistController::class, 'index'])->name('artist.index');
-Route::get('/artists/create', [ArtistController::class, 'create']);
+Route::get('/artists', [ArtistController::class, 'index'])->name('artist.index')->middleware('auth');
+Route::get('/artists/create', [ArtistController::class, 'create'])->name('artist.create')->middleware('auth');
 Route::post('/artists', [ArtistController::class, 'store'])->name('artists.store');
 Route::get('/artists/create', [ArtistController::class, 'create']);
 Route::get('/artists/{id}/edit', [ArtistController::class, 'edit']);
@@ -31,7 +31,10 @@ Route::get('/artists/{id}/delete', [ArtistController::class, 'delete']);
 Route::get('/songs/{id}/restore',  [SongController::class, 'restore'])->name('songs.restore');
 
 Route::view('/register', 'user.register');
+Route::view('/user/login', 'user.login');
 Route::post('/user/register', [UserController::class, 'register'])->name('user.register');
 Route::get('/user/profile', [UserController::class, 'profile'])->name('user.profile');
-Route::resource('albums', AlbumController::class);
-Route::resource('songs', SongController::class);
+Route::post('signin', [UserController::class, 'postSignin'])->name('user.signin');
+Route::get('logout', [UserController::class, 'logout'])->name('logout');
+Route::resource('albums', AlbumController::class)->middleware('auth')->except(['index']);
+Route::resource('songs', SongController::class)->middleware('auth');
