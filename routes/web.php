68423a8ff65e3976ec3ad887f703877675784806
rommel dalisay <rommelmdalisay@gmail.com>
2026-01-20 08:55:56 +0800

    route group

--- routes/web.php
+++ routes/web.php
@@ -21,20 +21,28 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/artists', [ArtistController::class, 'index'])->name('artist.index')->middleware('auth');
-Route::get('/artists/create', [ArtistController::class, 'create'])->name('artist.create')->middleware('auth');
-Route::post('/artists', [ArtistController::class, 'store'])->name('artists.store');
-Route::get('/artists/create', [ArtistController::class, 'create']);
-Route::get('/artists/{id}/edit', [ArtistController::class, 'edit']);
-Route::post('/artists/{id}/update', [ArtistController::class, 'update']);
-Route::get('/artists/{id}/delete', [ArtistController::class, 'delete']);
-Route::get('/songs/{id}/restore',  [SongController::class, 'restore'])->name('songs.restore');
+
 
 Route::view('/register', 'user.register');
 Route::view('/user/login', 'user.login');
 Route::post('/user/register', [UserController::class, 'register'])->name('user.register');
-Route::get('/user/profile', [UserController::class, 'profile'])->name('user.profile');
+
 Route::post('signin', [UserController::class, 'postSignin'])->name('user.signin');
-Route::get('logout', [UserController::class, 'logout'])->name('logout');
-Route::resource('albums', AlbumController::class)->middleware('auth')->except(['index']);
-Route::resource('songs', SongController::class)->middleware('auth');
+
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/artists', [ArtistController::class, 'index'])->name('artist.index');
+    Route::get('/artists/create', [ArtistController::class, 'create'])->name('artist.create');
+    Route::post('/artists', [ArtistController::class, 'store'])->name('artists.store');
+    Route::get('/artists/create', [ArtistController::class, 'create']);
+    Route::get('/artists/{id}/edit', [ArtistController::class, 'edit']);
+    Route::post('/artists/{id}/update', [ArtistController::class, 'update']);
+    Route::get('/artists/{id}/delete', [ArtistController::class, 'delete']);
+    Route::get('/songs/{id}/restore',  [SongController::class, 'restore'])->name('songs.restore');
+
+    Route::resource('songs', SongController::class);
+    Route::resource('albums', AlbumController::class);
+    Route::get('logout', [UserController::class, 'logout'])->name('logout');
+    Route::get('/user/profile', [UserController::class, 'profile'])->name('user.profile');
+});
